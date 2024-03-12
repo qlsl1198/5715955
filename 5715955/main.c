e42d3522c053f25c4827c2bd7792ee43e29b164b
kimsubin <kimsubin@2BB6>
2024-03-12 10:22:33 +0900

    5715955 kim subin 3_12

--- 5715955/main.c
+++ 5715955/main.c
@@ -2,7 +2,24 @@
 
 int main() {
 
-	printf("Hello World!\n");
+	int sum = 2;
+
+	for (int i = 3; i <= 100; i++) {
+		for (int j = 2; j < i; j++) {
+
+			if (j == i - 1) {
+				sum += i;
+			}
+			else if (i % j == 0) {
+				break;
+			}
+		
+		}
+	
+	}
+
+
+	printf("%d\n", sum);
 
 	return 0;
 }
